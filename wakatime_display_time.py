59d1dc18b1d13c7bd03df9700157d2c35e17f03e
Pavel <pavel.py3@gmail.com>
2019-04-12 17:56:57 +0300

    Some fixes.

--- wakatime_display_time.py
+++ wakatime_display_time.py
@@ -1,13 +1,24 @@
 from datetime import datetime
+import time
+import requests
+import sys
 from config import api_key
 
 URL = 'https://wakatime.com/api/v1/users/current/durations'
+
 
 def main():
     params = {'api_key': api_key}
     while True:
         params['date'] = datetime.today().strftime('%Y-%m-%d')
-        print(requies.get(URL, params))
+        req = requests.get(URL, params=params)
+        seconds = int(sum(i['duration'] for i in req.json()['data']))
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        sys.stdout.write('Coding progress: {:d}:{:02d}:{:02d}'.format(hours, minutes, seconds))
+        sys.stdout.flush()
+        time.sleep(60)
+
 
 if __name__ == '__main__':
     main()
